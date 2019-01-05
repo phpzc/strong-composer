3b2a8b941eb95edebb6fe18d409cbf087d8c26d6
phpzc <zhang5474jj@163.com>
2019-01-06 00:39:48 +0800

    下载安装composer 设置加速下载

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#下载composer
+wget https://getcomposer.org/download/1.8.0/composer.phar
+mv ./composer.phar /usr/local/bin/composer
+
+# 配置下载仓库
+composer config -g repo.packagist composer https://packagist.laravel-china.org
+
+# 安装加速
+composer global require hirak/prestissimo
+
+# 安装 流行开发包
