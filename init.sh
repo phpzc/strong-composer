2c740bcdfa5430add889fdba6cdf0bb8a1a336b7
zhangcheng <zhang5474jj@163.com>
2019-01-30 15:01:07 +0800

    add permission

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 #下载composer
 wget https://getcomposer.org/download/1.8.0/composer.phar
 mv ./composer.phar /usr/local/bin/composer
-
+chmod a+x /usr/local/bin/composer
 # 配置下载仓库
 composer config -g repo.packagist composer https://packagist.laravel-china.org
 
