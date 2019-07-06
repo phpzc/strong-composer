b47a2dd52ebc6e79cfe1a0738ce9d5f88c0f9723
phpzc <zhang5474jj@163.com>
2019-07-06 21:29:08 +0800

    修改镜像地址为阿里云地址

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ wget https://getcomposer.org/download/1.8.0/composer.phar
 mv ./composer.phar /usr/local/bin/composer
 chmod a+x /usr/local/bin/composer
 # 配置下载仓库
-composer config -g repo.packagist composer https://packagist.laravel-china.org
+composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
 
 # 安装加速
 composer global require hirak/prestissimo
